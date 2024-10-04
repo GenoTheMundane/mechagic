452c3ee136211ec23b30ebd3ead8f3ef60f37289
GenoTheMundane <75117749+GenoTheMundane@users.noreply.github.com>
2024-10-04 18:40:09 +0800

    retry

---
+++ _site/_assets/quote-gen.js
@@ -0,0 +1,89 @@
+quotes = [
+   "God may judge you but his sins outnumber your own <br> <b>-Cyberiamix-</b>",
+   "I am a monument to all your sins <br> <b>-Halo-</b>",
+   "I'll do whatever you want <br> Then Perish. <br> <b>-Obama Roleplay-</b>",
+   "Violence for Violence is the rule of the beasts  <br> <b>-Obama, from someone's dream-</b>",
+   "To become god is the loneliest achievement of them all <br> <b>-Obama, From Tumblr user's twofingerswhiskey's mother's dream-</b>",
+   "I survived because the fire inside me burned brighter than the fire around me <br> <b>-Fallout New Vegas NPC-</b>", 
+   "If the world chooses to become my enemy, I will fight just like I always have <br> <b>-Shadow The Hedgehog-</b>", 
+   "I will face god and walk backwards into hell <br> <b>-Twitter user @drill-</b>",
+   "All knowledge is based on that which we cannot prove. Will you fight? or will you perish like a dog? <br> <b>-Mickey Mouse comic edit-</b>",
+   "Every man's heart one day beats its final beat. His lungs breathe their final breath. And if what that man did in his life makes the blood pulse through the body of others and makes them bleed deeper in something that's larger than life, then his essence, his spirit, will be immortalized by the storytellers <br> <b>-Ultimate Warrior-</b>",
+   "Do you think god stays in heaven because he, too, lives in fear of what he's created? <br> <b>-Spykids-</b>",
+   "But what is stopping you? The best time to plant a tree was 20 years ago. The second best time is today <br> <b>-Greentext Anon-</b>",
+   "Nobody likes to change. There will always be resistance to change. And the quicker you get to that, the easier it is. It's not such a difficult thing. If you entrench yourself and go, 'by God, I will not change. I will not have this.' Then, you're a dead man. We're great at adaptability. It's our strongest suit <br> <b>-Nick Nolte-</b>",
+   "You've got to make a statement. You've got to look inside yourself and say: 'what am I willing to put up with today? <br> <b>-Erin, Game Grumps-</b>",
+   "Whenever you look at another creator or an artist that you respect, you're only seeing what took them a long time of work and doubt to push through. You never see the struggle behind it. So you think you're the only one struggling, when in fact, everyone goes through it <br> <b>-Danny, Gamegrumps-</b>",
+   "Too many people have opinions on things they know nothing about. And the more ignorant they are, the more opinions they have <br> <b>-Fallout NPC, Thomas Hildern-</b>",
+   "What is better- To be born good, or to overcome your evil nature through great effort <br> <b>-Skyrim, Paarthurnax-</b>",
+   "Pick a god and pray <br> <b>-Frederick, Fire Emblem Awakening-</b>",
+   "I see now that the circumstances of one's birth are irrelevant; it is what you do with the gift of life that determines who you are <br> <b>-Mewtwo, Pokemon-</b>",
+   "There's no point being grown-up if you can't be childish sometimes <br> <b>-Fourth Doctor, Doctor Who-</b>",
+   "Dude, sucking at something is the first step towards being sort of good at something <br> <b>-Jake the Dog, Adventure Time-</b>",
+   "Run for your Heterosexual lives, people! <br> <b>-Ted Ex talk-</b>",
+   "Tonight you spoke with the devil, the devil looked a lot like you <br> <b>-Tumblr Shitpost-</b>",
+   "Kill me. Kill me and live with the memory. Then tell the stars that you won <br> <b>-Warrior Cats-</b>",
+   "personsonable<br>me holding a gun to a mushroom: tell me the name of god you fungal piece of shit<br>mushroom: can you feel your heart burning? can you feel the struggle within? the fear within me is beyond anything your soul can make. you cannot kill me in a way that matters <br> me cocking the gun, tears streaming down my face: I'M NOT FUCKING SCARED OF YOU <br><br>miaislying<br>Hey OP? What the FUCK does this mean?<br><br>personsonable<br>decay exists as an extant form of life <br> <b>-personsonable, miaislying-</b>",
+   "What are you gonna do with that big bat? Gonna hit me? Better make it count. Better make it hurt. Better kill me in one shot <br> <b>-enodio_-</b>",
+   "One day you’ll decompose, and I’ll be there to watch it happen <br> <b>-turing-tested-</b>",
+   "I thought there were no heroes left in this world",
+   "You kneel before my throne unaware it was born on lies",
+   "Take this gift, for the gods surely won't",
+   "God gave me depression because if my ambitions went unchecked I would've bested him in hand to hand combat by age 16",
+   "I know you've all heard talk of being the emperor's chosen and heroes of the Imperium, but the truth is, you are essentially nothing to this Imperium. You're farmers, workers, and criminals, picked to be a part of the most expendable force known to man. You will be sent into battle. You will be sent often. Many of you will die, and you will die often. I do not expect you to like it, but I am asking that you accept this and make your peace with it.",
+   "My father taught me as a child that if you shoot for the moon and miss, the cold vacuum of space will suck out your eyeballs. Failure is not an option. Go kill them.",
+   "All these moments will be lost in time, like tears in the rain.",
+   "You are alone, child. There is only darkness for you, and only death for your people. These ancients are just the beginning. I will command a great and terrible army, and we will sail to a billion worlds. We will sail until every light has been extinguished. You are strong, child, but I am beyond strength. I am the end, and I have come for you.",
+   "To feel sorrow is to deserve peace.",
+   "No pet is perfect, it becomes perfect when you accept it for what it is.",
+   "You could sooner divert a river from its course than deny my nature",
+   "You cant shake the devil's hand and say you're only kidding.",
+   "No one will know the violence it took become this gentle.",
+   "Theres no light at the end of the tunnel so it's a good thing we brought matches.",
+   "It's not my job to pry, it's the government's.",
+   "And lo, he did reach into his bag of talent, and verily, it was found to be barren <br> <b>-Yamaha 10:99-</b>",
+   "Every comment is a prayer, bringing me closer to emerging from my cursed plane. Thank you for heralding the apocalypse this old god brings.",
+   "People say ''phase'' like impermanence means insignificance. Show me a permanent state of the self.",
+   "The answer to despair is action",
+   "You'll be reduced down to a single atom once I'm done with you",
+   "Hell is empty, and all the devils are here",
+   "There is not enough time to make all the things one’s imagination can conjure <br> <b>-reyndesign-</b>",
+   "I know this is a joke, but just remember that if you go knocking on enough doors asking to see the devil, eventually he may answer.",
+   "Why is religious Christmas imagery all so joyful and pleasant? where is the inherent horror of the birth of Christ? A mother is handed her newborn child, wailing and innocent. Her hands come away sticky. Red. Simply by giving her son life she has already killed him. He is doomed from the beginning. Her love will not save him from suffering. Because the thing cradled in her arms is not a baby, it is a sacrifice: born amongst the other bleating animals whose blood will one day be spilled in the name of what demands it. the night is silent with anticipation. Mary, did you know? That your womb was also a grave? <br> <b>-noknowshame-</b>",
+   "",
+
+
+            ];
+
+
+function generateprompt() {
+	var rand1;
+	rand1 = Math.floor((Math.random() * quotes.length) + 0); 
+    document.getElementById("prompt1").innerHTML = quotes[rand1];
+}
+
+function shuffle(quotes) {
+	let currentIndex = quotes.length, randomIndex;
+
+	// while there are items left to shuffle...
+	while (currentIndex != 0) {
+	// pick a remaining element...
+	randomIndex = Math.floor(Math.random() * currentIndex);
+
+	// decrease
+	currentIndex--;
+
+	// swap with current element
+	[quotes[currentIndex], quotes[randomIndex]] = [quotes[randomIndex], quotes[currentIndex]];
+	}
+	return quotes;
+}
+
+function displayLinks(num) {
+  shuffle(quotes);
+  
+  for (let i = 0; i < num; i++) {
+    var link = "<a href='" + quotes[i][0] + "'>" + quotes[i][1] + "</a><br>";
+    document.getElementById('randomPages').innerHTML += link;
+   }
+}
